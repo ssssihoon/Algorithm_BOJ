f6e42e8f9bdf03b9f178aef1822b2b84ebc3263d
Sihoon Hwang <ghkdtlgns207@naver.com>
2023-07-12 22:37:21 +0900

    [Bronze II] Title: 숫자의 개수, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,17 @@
+A = int(input())
+B = int(input())
+C = int(input())
+num = A*B*C
+num_li = list(str(num))
+print(num_li.count('0'))
+print(num_li.count('1'))
+print(num_li.count('2'))
+print(num_li.count('3'))
+print(num_li.count('4'))
+print(num_li.count('5'))
+print(num_li.count('6'))
+print(num_li.count('7'))
+print(num_li.count('8'))
+print(num_li.count('9'))
+
+
