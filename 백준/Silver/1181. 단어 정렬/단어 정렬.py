464d882b81a2f7f2e5409f32e6b89ecf3ee0c35e
Sihoon Hwang <ghkdtlgns207@naver.com>
2023-07-24 13:14:01 +0900

    [Silver V] Title: 단어 정렬, Time: 1028 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+
+words = [str(input()) for i in range(n)]
+
+words = list(set(words))
+words.sort()
+words.sort(key=len)
+
+for i in words:
+    print(i)
