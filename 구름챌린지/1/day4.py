08e362e40754001fcf01569f6259d60216639cd4
Sihoon Hwang <ghkdtlgns207@naver.com>
2023-08-18 00:04:07 +0900

    Update day4.py

--- 구름챌린지/1/day4.py
+++ 구름챌린지/1/day4.py
@@ -1,12 +1,3 @@
-'''
-햄버거의 맛은 사용된 모든 재료의 맛의 정도를 더한 값이다.
-완벽한 햄버거 -> 맛의 정도가 가장 높은 재료를 기준으로 위랑 아래로 갈수록 재료의 맛의 정도가 감소하거나 같아야 한다.
-'''
-
-'''
-7
-1 2 3 5 3 1 3
-'''
 import sys
 
 input = sys.stdin.readline
@@ -15,19 +6,19 @@ N = int(input())
 k = list(map(int, input().split()))
 sum_flavor = sum(k)
 max_k = max(k)
+turn = 0
 
 for i in range(N):
-    if i+1 < N and max_k >= k[i] and max_k != k[0] and max_k != k[-1]:
-        if k[i] <= k[i+1] and max_k >= k[i]:
+    if i+1 < N :
+        if k[0] == max_k:
+            turn = 1
+        if k[i] <= k[i+1] and turn == 0:
+            if k[i+1] == max_k:
+                turn = 1
             continue
-        elif k[i] >= k[i+1] and max_k <= k[i]:
+        elif k[i] >= k[i+1] and turn == 1:
             continue
         else:
             sum_flavor = 0
-            pass
+            break
 print(sum_flavor)
-
-'''
-처음과 마지막의 재료가 max값이면 안된다.
-max_k != k[0] and max_k != k[-1]
-'''
