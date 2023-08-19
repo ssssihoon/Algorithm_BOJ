b4adf68301640cde2d3450ccd10ea1d6a1be9521
Sihoon Hwang <ghkdtlgns207@naver.com>
2023-08-19 17:29:30 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+L = int(input())
+string = input()
+result = 0
+
+for i in range(L):
+    result += (ord(string[i])-96) * (31 ** i)
+print(result % 1234567891)
